cc783ad25d7ffdbcadd9f9de6fd146661feb8282
Quentin Berthet <quentin@berthet.ch>
2024-08-06 13:22:07 +0200

    project frontend init

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import '../styles/Header.css'
+
+const Header = () => {
+    return(
+        <>
+            <div className="Header">
+                <p>Chat App by <a href={"https://github.com/BerthetQuentin"}>Quentin Berthet</a></p>
+            </div>
+        </>
+    )
+}
+
+export default Header;
